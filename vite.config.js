2009ca955b3f7298d61697db29e752824a0298bd
Muhammed Sina Gün <145281408+msgxr@users.noreply.github.com>
2025-09-04 17:39:25 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Bist-Excel-App/",   // repo adınızı birebir yazın
+  base: "/bist-excel-app/",   // repo adınızı birebir yazın
 })
