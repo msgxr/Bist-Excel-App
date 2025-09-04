ff9a7d6257ffa5abff5dc6b45e68252d0b6d6b48
Muhammed Sina <145281408+msgxr@users.noreply.github.com>
2025-09-04 17:30:52 +0300

    fix: vite base ayarı eklendi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Bist-Excel-App/",   // repo adınızı birebir yazın
 })
